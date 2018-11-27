bcd81cb0db38cc26e21cbcbbd37d818adad27a90
Lindsey Nguyen <lnguyent01@169-231-220-217.wireless.ucsb.edu>
2018-11-26 20:25:10 -0800

    added skeleton Customer.java class

---
+++ src/Customer.java
@@ -0,0 +1,27 @@
+
+
+
+public class Customer {
+	private String name;
+	private String taxID;
+	private String address;
+	private String pin;
+	private Int[] accountIds;
+
+	public Customer(String name, String taxID, String address, String pin, int ) {
+
+	}
+
+	public boolean VerifyPIN(String PIN) {
+
+	}
+
+	public void SetPIN(OldPIN, NewPIN) {
+
+	}
+
+	public void Purchase(accountID, amount) {
+
+	}
+
+}
